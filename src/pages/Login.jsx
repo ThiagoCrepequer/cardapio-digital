60e2f23a6393622eed9f5ccd2fd636d795b86a02
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-12-07 21:31:09 -0300

    feat: criado logica da pagina de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,34 @@
+import Cookies from "js-cookie";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 export function Login() {
+    const navigate = useNavigate();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        const resposta = await axios.post(import.meta.env.VITE_API_URL + "/auth/login", {
+            email: e.target[0].value,
+            password: e.target[1].value,
+        });
+
+        if(resposta.status !== 200) {
+            alert("Erro ao fazer login");
+            return;
+        }
+
+        const responseToken = resposta.headers.authorization;
+        const token = responseToken.replace("Bearer ", "");
+        Cookies.set("token", token);
+    
+        return navigate("/admin");
+    }
+
     return (
-        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <button type="submit">Login</button>
+        </form>
     )
 }
---
+++ src/services/axios.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
+});
+
+instance.interceptors.request.use((config) => {
+    const token = Cookies.get('token');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+})
+
+export default instance;
